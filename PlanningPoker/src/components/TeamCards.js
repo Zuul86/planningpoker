4265c627527fdd0d8c3df44aefa63abbd2796dc8
Adam Pritzl <zuul86@gmail.com>
2017-05-19 23:19:37 -0500

    Group selected cards by effort

--- PlanningPoker/src/components/TeamCards.js
+++ PlanningPoker/src/components/TeamCards.js
@@ -1,11 +1,30 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
+import '../styles/mycard.css';
+
+function groupCards(cards) {
+    const groupedArray = [];
+    cards.forEach((card) => {
+        const foundCard = groupedArray.find(item => {
+            item = item || {};
+            return item.Effort === card.Effort;
+        });
+        if (foundCard) {
+            foundCard.Count++;
+        } else {
+            card.Count = 1;
+            groupedArray.push(card);
+        }
+    });
+
+    return groupedArray;
+}
 
 const TeamCards = (props) => {
         return (
             <div>
                 <div>Number of team members: {props.numberOfClients}</div>
-                {props.cards.map((item)=> {
-                    return <div key={item.UserId.toString()}>{item.Effort}</div>;
+                {groupCards(props.cards).map((item)=> {
+                    return <div className="card" key={item.UserId.toString()}>{item.Effort}</div>;
                 })}
             </div>);
 };
