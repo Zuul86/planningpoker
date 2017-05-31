64b814c670a07623a32ce4573dcb7472c55b7d5b
Adam Pritzl <zuul86@gmail.com>
2017-05-31 11:04:29 -0500

    Add mimetype for woff/woff2

--- PlanningPoker/src/components/MyCards.js
+++ PlanningPoker/src/components/MyCards.js
@@ -5,7 +5,7 @@ import '../styles/mycards.css';
 const MyCards = ({onCardClick}) => {
     return (
         <div className="bottom-panel">
-            {[.5, 1, 2, 3, 5, 8, 13, 21 ].map(item=>{
+            {[.5, 1, 2, 3, 5, 8, 13, 20 ].map(item=>{
                 return <MyCard key={item.toString()} cardNumber={item} onCardClick={onCardClick} />;
             })}
         </div>
--- PlanningPoker/webpack.config.dev.js
+++ PlanningPoker/webpack.config.dev.js
@@ -30,7 +30,6 @@ export default {
       {test: /\.(js|jsx?)$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
       {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
-      //figure out how to load fonts
       {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
