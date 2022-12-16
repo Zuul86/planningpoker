8ca3ad2586b7813fae0e1520e8f5e689d427c1d4
Adam <Zuul86@users.noreply.github.com>
2022-12-16 11:12:15 -0600

    Action bar (#2)
    * action bar layout
    
    * Add jointable functionality

--- web/planning-poker-app/src/App.tsx
+++ web/planning-poker-app/src/App.tsx
@@ -1,15 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 import MyCards from './components/MyCards/MyCards'
 
 function App() {
+
+  const [tableName, setTableName] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const mySocket = new WebSocket("wss://733l6u90dc.execute-api.us-west-2.amazonaws.com/dev");
+
+  function joinTable() {
+    const myAction = {
+      action: 'jointable',
+      tableName: tableName,
+      userName: userName
+    }
+    mySocket.send(JSON.stringify(myAction));
+  }
+
   return (
-    <div>
+    <div className='container'>
+      <div className='top'>
+        <div>
+          <label>Table Name:<input name='tableName' type='text' onChange={(e) => setTableName(e.target.value)} /></label>
+          <label>Name:<input name='userName' type='text' onChange={(e) => setUserName(e.target.value)} /></label>
+          <button type='button' onClick={joinTable}>Join Table</button>
+        </div>
+        <div>
+          <button type='button'>Reveal</button>
+          <button type='button'>Reset Votes</button>
+        </div>
+      </div>
       <div></div>
       <div className='bottom'>
         <MyCards></MyCards>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
