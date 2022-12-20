ddcacfd29cea11848a6d1afc8cd537598100e3c7
Adam <Zuul86@users.noreply.github.com>
2022-12-20 15:17:18 -0600

    check old table for deletes (#6)

--- infrastructure/lambdas/notifyjoined/index.ts
+++ infrastructure/lambdas/notifyjoined/index.ts
@@ -8,9 +8,11 @@ export const handler = async (event: DynamoDBStreamEvent, context: Context) => {
     const apiGatewayClient = new ApiGatewayManagementApi({ endpoint: 'https://733l6u90dc.execute-api.us-west-2.amazonaws.com/dev' })
 
     for (const record of event.Records) {
-        const tablejoined = record.dynamodb?.NewImage?.TableName.S || ''
+        let tablejoined = record.dynamodb?.NewImage?.TableName.S || ''
         
-        if(!tablejoined){ continue; }
+        if(!tablejoined){
+            tablejoined = record.dynamodb?.OldImage?.TableName.S || ''
+         }
 
         const queryParams: QueryCommandInput = {
             TableName: 'PlanningPokerTable',
