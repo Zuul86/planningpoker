103bd71780ba219d7400b99e94d9e5bfffd85904
Adam <zuul86@gmail.com>
2026-03-20 16:47:17 +0000

    clean up votes, consolidate logic

--- infrastructure/lambdas/disconnect/index.ts
+++ infrastructure/lambdas/disconnect/index.ts
@@ -5,32 +5,37 @@ import { unmarshall } from '@aws-sdk/util-dynamodb';
 export const handler = async (event: APIGatewayEvent, context: Context) : Promise<APIGatewayProxyResult> => {
     const client = new DynamoDBClient({ region: "us-west-2" });
 
-    //This is not scalable but will do for what I need.
-    const scanFilter: ScanCommandInput = {
-        TableName: 'PlanningPokerTable',
-        FilterExpression: "ConnectionId = :connectionId",
-        ExpressionAttributeValues: { ":connectionId": { S: event.requestContext.connectionId || '' } }
-    }
-
-    const recordsToDelete = await client.send(new ScanCommand(scanFilter))
-    const numberOfRecordsToDelete = recordsToDelete.Count || 0;
-
-    for (let i = 0; i < numberOfRecordsToDelete; i++) {
-        const item = recordsToDelete.Items ? recordsToDelete.Items[i] : {};
-        const itemToDelete = unmarshall(item);
-
-        const params: DeleteItemCommandInput = {
-            TableName: 'PlanningPokerTable',
-            Key: { 'TableName': { S: itemToDelete.TableName }, 'ConnectionId': { S: String(itemToDelete.ConnectionId) } }
-        };
-
-        try {
-            const response = await client.send(new DeleteItemCommand(params));
-            console.log(response.$metadata);
-        } catch (e) {
-            console.log(e)
+    const deleteByConnectionId = async (tableName: string) => {
+        //This is not scalable but will do for what I need.
+        const scanFilter: ScanCommandInput = {
+            TableName: tableName,
+            FilterExpression: "ConnectionId = :connectionId",
+            ExpressionAttributeValues: { ":connectionId": { S: event.requestContext.connectionId || '' } }
         }
-    }
+    
+        const recordsToDelete = await client.send(new ScanCommand(scanFilter))
+        const numberOfRecordsToDelete = recordsToDelete.Count || 0;
+    
+        for (let i = 0; i < numberOfRecordsToDelete; i++) {
+            const item = recordsToDelete.Items ? recordsToDelete.Items[i] : {};
+            const itemToDelete = unmarshall(item);
+    
+            const params: DeleteItemCommandInput = {
+                TableName: tableName,
+                Key: { 'TableName': { S: itemToDelete.TableName }, 'ConnectionId': { S: String(itemToDelete.ConnectionId) } }
+            };
+    
+            try {
+                const response = await client.send(new DeleteItemCommand(params));
+                console.log(response.$metadata);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    };
+
+    await deleteByConnectionId('PlanningPokerTable');
+    await deleteByConnectionId('PlanningPokerVote');
 
     return {
         statusCode: 200,
